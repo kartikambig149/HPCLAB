ba204b1164ae9ca20b238014d98526c359b70825
Kartik Umesh Ambig <149179934+kartikambig149@users.noreply.github.com>
2024-12-17 19:50:52 +0530

    Create p6.c

---
+++ p6.c
@@ -0,0 +1,25 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char** argv) {
+    int rank, size, mangoes, total_mangoes;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    srand(time(NULL) + rank);
+    mangoes = rand() % 100 + 1;
+    printf("Robot %d picked %d mangoes.\n", rank, mangoes);
+
+    MPI_Reduce(&mangoes, &total_mangoes, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        printf("Total mangoes picked by %d robots: %d\n", size, total_mangoes);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
